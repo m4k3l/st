41ff9e14086eaaddf9d84c07831c8e125f066346
Markus Hanetzok <markus@hanetzok.net>
2023-05-14 02:25:47 +0200

    Changed colors

--- config.def.h
+++ config.def.h
@@ -114,34 +114,34 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    [0] = "#000000", /* black   */
-    [1] = "#ff5555", /* red     */
-    [2] = "#50fa7b", /* green   */
-    [3] = "#f1fa8c", /* yellow  */
-    [4] = "#bd93f9", /* blue    */
-    [5] = "#ff79c6", /* magenta */
-    [6] = "#8be9fd", /* cyan    */
-    [7] = "#bbbbbb", /* white   */
+    [0] = "#373737", /* black   */
+    [1] = "#ec5f67", /* red     */
+    [2] = "#99c794", /* green   */
+    [3] = "#fac863", /* yellow  */
+    [4] = "#6699cc", /* blue    */
+    [5] = "#c594c5", /* magenta */
+    [6] = "#62b2b3", /* cyan    */
+    [7] = "#d8dee9", /* white   */
 
     /* 8 bright colors */
-    [8]  = "#44475a", /* black   */
-    [9]  = "#ff5555", /* red     */
-    [10] = "#50fa7b", /* green   */
-    [11] = "#f1fa8c", /* yellow  */
-    [12] = "#bd93f9", /* blue    */
-    [13] = "#ff79c6", /* magenta */
-    [14] = "#8be9fd", /* cyan    */
-    [15] = "#ffffff", /* white   */
+    [8]  = "#bbbbbb", /* black   */
+    [9]  = "#ec5f67", /* red     */
+    [10] = "#99c794", /* green   */
+    [11] = "#fac863", /* yellow  */
+    [12] = "#6699cc", /* blue    */
+    [13] = "#c594c5", /* magenta */
+    [14] = "#62b2b3", /* cyan    */
+    [15] = "#d8dee9", /* white   */
 
     /* special colors */
-    [256] = "#282a36", /* background */
-    [257] = "#f8f8f2", /* foreground */
+    [256] = "#1e1e1e", /* background */
+    [257] = "#f7f7f7", /* foreground */
 };
 
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor
+ * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
